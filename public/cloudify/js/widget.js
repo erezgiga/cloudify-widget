a6beae7b36272c8f8036ed403b9a6645980cca39
erezgiga <erez@gigaspaces.com>
2013-07-16 16:53:29 +0300

    postMessage/recieveMessage POC

--- public/cloudify/js/widget.js
+++ public/cloudify/js/widget.js
@@ -177,7 +177,7 @@ $(function () {
     var params = {};
     var hash;
     var hashes = window.location.search.substring(1).split('&');
-    console.log(["using search tearm", hashes ]);
+    //console.log(["using search tearm", hashes ]);
     for(var i = 0; i < hashes.length; i++)
     {
       hash = hashes[i].split('=');
