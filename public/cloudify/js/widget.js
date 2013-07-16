a75ae0667b1e8c33f0fe438d32f010d65cf97b3c
erezgiga <erez@gigaspaces.com>
2013-07-16 16:58:21 +0300

    postMessage/recieveMessage POC

--- public/cloudify/js/widget.js
+++ public/cloudify/js/widget.js
@@ -177,7 +177,7 @@ $(function () {
     var params = {};
     var hash;
     var hashes = window.location.search.substring(1).split('&');
-    //console.log(["using search tearm", hashes ]);
+    console.log(["using search tearm", hashes ]);
     for(var i = 0; i < hashes.length; i++)
     {
       hash = hashes[i].split('=');
@@ -501,7 +501,7 @@ $(function () {
     function start_instance_btn_handler()
     {
         var myUrl = origin_page_url;
-        //console.log(["sending message", myUrl, parent ] );
+        console.log(["sending message", myUrl, parent ] );
         //console.log("after message");
         if ( is_requires_login() && !params["userId"] ){
             $.postMessage( JSON.stringify({name:"require_login"}), myUrl , parent );
